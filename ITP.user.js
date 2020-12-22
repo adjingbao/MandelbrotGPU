ed6dacd892216960566ef05ad4bbd12396147526
adjingbao <104521120+adjingbao@users.noreply.github.com>
2020-12-22 15:19:31 +0000

    fix share

--- ITP.user.js
+++ ITP.user.js
@@ -3,4 +3,4 @@
 // @namespace  http://thomshutt.com/greasemonkey
 // @version    0.1
 // @description Reminds you to use the ITP Amazon link 
-// @include    http*://amazon.
+// @include    http*://amazon.*
