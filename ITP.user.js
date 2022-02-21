5c0ab28e8edb2ce0116be641ca512a1d20f17da2
adjingbao <104521120+adjingbao@users.noreply.github.com>
2022-02-21 21:50:05 +0000

    Fixed it.

--- ITP.user.js
+++ ITP.user.js
@@ -14,4 +14,4 @@ var node = document.body;
 var infoDiv = document.createElement('div');
 infoDiv.style.width= "100%";
 infoDiv.style.backgroundColor = "orange";
-infoDiv.innerHTML = "<center>HAVE YOU USED A REFERRAL LINK
+infoDiv.innerHTML = "<center>HAVE YOU USED A REFERRAL LINK?
